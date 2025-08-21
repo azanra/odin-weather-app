11245e34a3df99828d09fd7685ee9e66a533480d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-08-21 09:50:40 +0700

    refactor: remove boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./style.css";
-import img from "./asset/resource/img.png";
-
-console.log("Hello world!");
-const paraElement = document.querySelector("p");
-const imgElement = document.createElement("img");
-imgElement.src = img;
-paraElement.appendChild(imgElement);
